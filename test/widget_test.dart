231ccaaa76f48734d326a460f8483eef32c60dac
Momin Rohan <mominrohan30@gmail.com>
2025-09-06 00:09:40 +0500

    launcher icon added pubspec.yaml

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:oneals_app/main.dart';
+import 'package:path_to_purpose/main.dart';
 
 void main() {
   testWidgets('App loads without crashing', (WidgetTester tester) async {
